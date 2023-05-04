357b5807f6adb52a9362f0227d6a0cb17d54114e
xiayuanyuan <xiayuanyuan_yewu@cmss.chinamobile.com>
2023-05-04 14:22:31 +0800

    add oe_test_chmod for pre_test

---
+++ sut/env_check/smoke_test/local_sh/oe_test_chmod_001.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/bash
+
+# Copyright: Copyright (c) Huawei Technologies Co., Ltd. 2023-2023. All rights reserved.
+# Create: 2023-02-09
+
+OET_PATH=$(
+    cd "$(dirname "$0")" || exit 1
+    pwd
+)
+source "$OET_PATH/../common/common_lib.sh"
+# 环境准备
+function pre_test() {
+    LOG_INFO "Start environment preparation."
+    OLD_LANG=$LANG
+    export LANG=en_US.UTF-8
+    ls /tmp/test1 && rm -rf /tmp/test1
+    LOG_INFO "End of environmental preparation!"
+}
+
+main $@
