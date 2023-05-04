ef99d43cd6dce7badd2298587506ff1c7dea7cc4
xiayuanyuan <xiayuanyuan_yewu@cmss.chinamobile.com>
2023-05-04 14:30:22 +0800

    add oe_test_chmod for run_test

--- sut/env_check/smoke_test/local_sh/oe_test_chmod_001.sh
+++ sut/env_check/smoke_test/local_sh/oe_test_chmod_001.sh
@@ -16,5 +16,30 @@ function pre_test() {
     ls /tmp/test1 && rm -rf /tmp/test1
     LOG_INFO "End of environmental preparation!"
 }
+# 用例执行
+function run_test() {
+    LOG_INFO "Start testing......"
+    mkdir -p /tmp/test1/test2/test3
+    ls -l /tmp | grep "test1" | awk -F ' ' '{print $1}' | grep "drwxr-xr-x"
+    CHECK_RESULT $?
+    per01=$(ls -l /tmp/test1 | grep "test2" | awk -F ' ' '{print $1}')
+    #check chmod
+    chmod 777 /tmp/test1
+    ls -l /tmp | grep "test1" | awk -F ' ' '{print $1}' | grep "drwxrwxrwx"
+    CHECK_RESULT $?
+    per02=$(ls -l /tmp/test1 | grep "test2" | awk -F ' ' '{print $1}')
+    [ "$per01" == "$per02" ]
+    CHECK_RESULT $?
+    # check chmod -R
+    chmod -R 777 /tmp/test1
+    ls -l /tmp/ | grep "test1" | awk -F ' ' '{print $1}' | grep "drwxrwxrwx"
+    CHECK_RESULT $?
+    ls -l /tmp/test1 | grep "test2" | awk -F ' ' '{print $1}' | grep "drwxrwxrwx"
+    CHECK_RESULT $?
+    #check chmod help
+    chmod --help | grep -E "Usage|用法"
+    CHECK_RESULT $?
+    LOG_INFO "Finish test!"
+}
 
 main $@
