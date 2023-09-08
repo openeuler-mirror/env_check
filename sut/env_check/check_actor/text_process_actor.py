a20a2158bde97816470949ebc8d37e253eff688b
wangdong <wangdong_yewu@cmss.chinamobile.com>
2023-09-08 17:38:07 +0800

    add col and colrm command for text process

--- sut/env_check/check_actor/text_process_actor.py
+++ sut/env_check/check_actor/text_process_actor.py
@@ -9,3 +9,13 @@ class OeCmpActor(OeBaseActor):
     name = "OeCmpActor"
     description = "Test the cmp cmmand of the openEuler system."
     sh_file = '03_text_process/oe_test02_cmp.sh'
+
+class OeColActor():
+    name = "OeColActor"
+    destription = ""
+    sh_file = "03_text_process/oe_test03_col.sh"
+
+class OeColrmActor():
+    name = "OeColrmActor"
+    destription = ""
+    sh_file = "03_text_process/oe_test04_colrm.sh"
