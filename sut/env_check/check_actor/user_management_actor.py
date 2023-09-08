4618c56551a2b24b52762e5a25913a85c3c33311
zengyifeng <zengyifeng_yewu@cmss.chinamobile.com>
2023-09-08 15:25:10 +0800

    add groupadd

--- sut/env_check/check_actor/user_management_actor.py
+++ sut/env_check/check_actor/user_management_actor.py
@@ -6,3 +6,10 @@ class OeGpasswdActor(OeBaseActor):
     name = "OeGpasswdActor"
     description = "Test the gpasswd command of the openEuler system."
     sh_file = '07_user_management/oe_test07_gpasswd.sh'
+    
+
+class OeGroupaddActor(OeBaseActor):
+    name = "OeGroupaddActor"
+    description = "Test the groupadd command of the openEuler system."
+    sh_file = '07_user_management/oe_test07_groupadd.sh'    
+
