e0411389d2909ac3dd787604a4990b480e1c3c53
zengyifeng <zengyifeng_yewu@cmss.chinamobile.com>
2023-09-22 13:04:07 +0800

    Rename bash script

--- sut/env_check/check_actor/user_management_actor.py
+++ sut/env_check/check_actor/user_management_actor.py
@@ -1,49 +1,50 @@
 # -*- coding: utf-8 -*-
+# Create: 2023-08-31
+# Author: zengyifeng
 
-from sut.env_check.check_actor.oe_base_actor import OeBaseActor    
+from sut.env_check.check_actor.oe_base_actor import OeBaseActor
     
 class OeGpasswdActor(OeBaseActor):
     name = "OeGpasswdActor"
     description = "Test the gpasswd command of the openEuler system."
-    sh_file = '07_user_management/oe_test07_gpasswd.sh'
-    
+    sh_file = '07_user_management/oe_test01_gpasswd.sh'
 
 class OeGroupaddActor(OeBaseActor):
     name = "OeGroupaddActor"
     description = "Test the groupadd command of the openEuler system."
-    sh_file = '07_user_management/oe_test07_groupadd.sh'    
+    sh_file = '07_user_management/oe_test02_groupadd.sh'    
 
 class OeGroupdelActor(OeBaseActor):
     name = "OeGroupdelActor"
     description = "Test the groupdel command of the openEuler system."
-    sh_file = '07_user_management/oe_test07_groupdel.sh'
-    
+    sh_file = '07_user_management/oe_test03_groupdel.sh'
+
 class OeGroupmodActor(OeBaseActor):
     name = "OeGroupmodActor"
     description = "Test the groupmod command of the openEuler system."
-    sh_file = '07_user_management/oe_test07_groupmod.sh'
-    
+    sh_file = '07_user_management/oe_test04_groupmod.sh'
+
 class OeGroupsActor(OeBaseActor):
     name = "OeGroupsActor"
     description = "Test the groups command of the openEuler system."
-    sh_file = '07_user_management/oe_test07_groups.sh'
-    
+    sh_file = '07_user_management/oe_test05_groups.sh'
+
 class OeGrpckActor(OeBaseActor):
     name = "OeGrpckActor"
     description = "Test the grpck command of the openEuler system."
-    sh_file = '07_user_management/oe_test07_grpck.sh'
-    
+    sh_file = '07_user_management/oe_test06_grpck.sh'
+
 class OeGrpconvActor(OeBaseActor):
     name = "OeGrpconvActor"
     description = "Test the grpconv command of the openEuler system."
     sh_file = '07_user_management/oe_test07_grpconv.sh'
-    
+
 class OeGrpunconvActor(OeBaseActor):
     name = "OeGrpunconvActor"
     description = "Test the grpunconv command of the openEuler system."
-    sh_file = '07_user_management/oe_test07_grpunconv.sh'
+    sh_file = '07_user_management/oe_test08_grpunconv.sh'
 
 class OeLognameActor(OeBaseActor):
     name = "OeLognameActor"
     description = "Test the logname command of the openEuler system."
-    sh_file = '07_user_management/oe_test07_logname.sh'
+    sh_file = '07_user_management/oe_test09_logname.sh'
