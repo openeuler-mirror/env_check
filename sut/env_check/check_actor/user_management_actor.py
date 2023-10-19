c028e57de4c89993377828da5f816a5ae60e3fc4
zengyifeng <zengyifeng_yewu@cmss.chinamobile.com>
2023-10-19 10:37:03 +0800

    add usermod command check actor and bash script

--- sut/env_check/check_actor/user_management_actor.py
+++ sut/env_check/check_actor/user_management_actor.py
@@ -79,8 +79,12 @@ class OeUseraddActor(OeBaseActor):
     description = "Test the useradd command of the openEuler system."
     sh_file = '07_user_management/oe_test15_useradd.sh'
 
-
 class OeUserdelActor(OeBaseActor):
     name = "OeUserdelActor"
     description = "Test the userdel command of the openEuler system."
     sh_file = '07_user_management/oe_test16_userdel.sh'
+
+class OeUsermodActor(OeBaseActor):
+    name = "OeUsermodActor"
+    description = "Test the usermod command of the openEuler system."
+    sh_file = '07_user_management/oe_test17_usermod.sh'
