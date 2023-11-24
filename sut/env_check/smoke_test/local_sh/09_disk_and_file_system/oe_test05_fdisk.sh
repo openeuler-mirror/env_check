003f18805ae3799a6af1178f324bc89f367aba44
hebinxin <hebinxin_yewu@cmss.chinamobile.com>
2023-11-24 13:56:44 +0800

    add fdisk command scirpt and actor

---
+++ sut/env_check/smoke_test/local_sh/09_disk_and_file_system/oe_test05_fdisk.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/bash
+# Create: 2023-11-24 13:52:20
+# Auther: hebinxin
+# Description: A shellscript to check fdisk command.
+
+OET_PATH=$(
+    cd "$(dirname "$0")" || exit 1
+    pwd
+)
+source "$OET_PATH/../../common/common_lib.sh"
+
+# 环境准备
+function pre_test() {
+    LOG_INFO "Start to prepare the test environment."
+    OLD_LANG=$LANG
+    export LANG=en_US.UTF-8
+    LOG_INFO "End to prepare the test environment."
+}
+
+# 用例执行
+function run_test() {
+    LOG_INFO "Start testing..."
+
+    # 1. test fdisk -l
+    fdisk -l
+    CHECK_RESULT $? 0 0 "fdisk -l error"
+
+    # 2. test fdisk -h
+    fdisk -h | grep -E "Usage|用法"
+    CHECK_RESULT $? 0 0 "fdisk -h error"
+
+    LOG_INFO "Finish test!"
+}
+
+环境清理
+function post_test() {
+    LOG_INFO "Start to restore the test environment."
+    export LANG=${OLD_LANG}
+    LOG_INFO "End to restore the test environment."
+}
+
+main $@
+
