bf8b48063039b9e12d7d29fde1807a3206a6ecf9
pengtengfei <pengtengfei_yewu@cmss.chinamobile.com>
2023-12-08 17:20:23 +0800

    add script for command arch

---
+++ sut/env_check/check_actor/system_management_actor.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+from sut.env_check.check_actor.oe_base_actor import OeBaseActor
+
+class OeArchActor(OeBaseActor):
+    name = "OeArchActor"
+    description = "Test the arch command of the openEuler system."
+    sh_file = '14_system_management/oe_test01_arch.sh'
