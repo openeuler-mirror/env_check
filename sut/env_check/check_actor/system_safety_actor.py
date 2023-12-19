cca7ffe8011ae9f78b5f29d721db96e1aa3bbc50
zengyifeng <zengyifeng_yewu@cmss.chinamobile.com>
2023-12-19 14:24:10 +0800

    add nmap command check actor and bash script

--- sut/env_check/check_actor/system_safety_actor.py
+++ sut/env_check/check_actor/system_safety_actor.py
@@ -8,3 +8,8 @@ class OeChrootActor(OeBaseActor):
     name = "OeChrootActor"
     description = "Test the chroot command of the openEuler system."
     sh_file = '12_system_safety/oe_test01_chroot.sh'
+
+class OeNmapActor(OeBaseActor):
+    name = "OeNmapActor"
+    description = "Test the nmap command of the openEuler system."
+    sh_file = '12_system_safety/oe_test02_nmap.sh'
