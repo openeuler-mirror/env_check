c3dbad1a984b40b64cf8855f91e8e8433a81e1d4
dongchao <dongchao_yewu@cmss.chinamobile.com>
2024-01-12 10:01:48 +0800

    add code for command modprobe

---
+++ sut/env_check/smoke_test/local_sh/10_kernelcmd_directories/oe_test07_modprobe.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/bash -x
+
+# Create: 2023-09-019
+
+OET_PATH=$(
+    cd "$(dirname "$0")" || exit 1
+    pwd
+)
+source "$OET_PATH/../../common/common_lib.sh"
+# 环境准备
+function pre_test() {
+    LOG_INFO "Start to prepare the test environment."
+    OLD_LANG=$LANG
+    export LANG=en_US.UTF-8
+    LOG_INFO "End to prepare the test environment."
+}
+# 用例执行
+function run_test() {
+    LOG_INFO "Start testing..."
+    modprobe iptable_nat  
+    CHECK_RESULT $?
+	LOG_INFO "Finish test!"
+}
+# 环境清理
+function post_test() {
+    LOG_INFO "Start to restore the test environment."
+    export LANG=${OLD_LANG}
+    LOG_INFO "End to restore the test environment."
+}
+
+main $@
