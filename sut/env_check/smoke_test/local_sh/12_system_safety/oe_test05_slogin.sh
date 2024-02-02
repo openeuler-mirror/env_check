32b844c871edd40a1bbabe9a5b39540769043078
zengyifeng <zengyifeng_yewu@cmss.chinamobile.com>
2024-02-02 17:26:42 +0800

    add slogin command bash script

---
+++ sut/env_check/smoke_test/local_sh/12_system_safety/oe_test05_slogin.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/bash
+
+# Create: 2023-12-19
+
+OET_PATH=$(
+    cd "$(dirname "$0")" || exit 1
+    pwd
+)
+source "$OET_PATH/../../common/common_lib.sh"
+
+
+# 环境准备
+function pre_test() {
+    LOG_INFO "Start to prepare the test environment."
+    OLD_LANG=$LANG
+    export LANG=en_US.UTF-8
+    LOG_INFO "End to prepare the test environment."
+}
+
+
+# 用例执行
+function run_test() {
+    LOG_INFO "Start testing..."
+
+    # check whether slogin is or not installed
+    if ! command -v slogin &> /dev/null; then
+        LOG_WARN "slogin command is not installed"
+        CHECK_RESULT $? 0 0
+    fi
+
+
+    LOG_INFO "Finish test!"
+}
+
+
+
+
+
+# 环境清理
+function post_test() {
+    LOG_INFO "Start to restore the test environment."
+    export LANG=${OLD_LANG}
+    LOG_INFO "End to restore the test environment."
+}
+
+main $@
