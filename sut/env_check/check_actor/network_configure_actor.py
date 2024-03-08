3b7a1288c2532ff261b2a51a82fc55506fd5b958
王栋 <wangdong_yewu@cmss.chinamobile.com>
2024-03-08 16:42:25 +0800

    add ifdown command for network configure

--- sut/env_check/check_actor/network_configure_actor.py
+++ sut/env_check/check_actor/network_configure_actor.py
@@ -24,3 +24,8 @@ class OeIfconfigActor(OeBaseActor):
     name = "OeIfconfigActor"
     description = "Test the ifconfig cmmand of the openEuler system."
     sh_file = "15_network_configure/oe_test05_ifconfig.sh"
+
+class OeIfdownActor(OeBaseActor):
+    name = "OeIfdownActor"
+    description = "Test the ifdown cmmand of the openEuler system."
+    sh_file = "15_network_configure/oe_test06_ifdown.sh"
