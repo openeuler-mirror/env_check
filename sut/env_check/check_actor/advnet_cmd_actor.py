3fbd368ca633b2b91ba1e21924e1f563a561727e
dongchao <dongchao_yewu@cmss.chinamobile.com>
2024-03-25 09:52:01 +0800

    add code for advnet_cmd_actor.py

---
+++ sut/env_check/check_actor/advnet_cmd_actor.py
@@ -0,0 +1,20 @@
+class OeAbActor(OeBaseActor):
+    name = "OeAbActor"
+    description = "Test the ab command of the openEuler system."
+    sh_file = '18_netservcmd_directories/oe_test01_ab.sh'	
+class OeApachectlActor(OeBaseActor):
+    name = "OeApachectlActor"
+    description = "Test the apachectl command of the openEuler system."
+    sh_file = '18_netservcmd_directories/oe_test02_apachectl.sh'
+class OeExportfsActor(OeBaseActor):
+    name = "OeExportfsActor"
+    description = "Test the exportfs command of the openEuler system."
+    sh_file = '18_netservcmd_directories/oe_test03_exportfs.sh'
+class OeHttpdActor(OeBaseActor):
+    name = "OeHttpdActor"
+    description = "Test the httpd command of the openEuler system."
+    sh_file = '18_netservcmd_directories/oe_test04_httpd.sh'	
+class OeSshdActor(OeBaseActor):
+    name = "OeSshdActor"
+    description = "Test the sshd command of the openEuler system."
+    sh_file = '18_netservcmd_directories/oe_test05_sshd.sh'	
