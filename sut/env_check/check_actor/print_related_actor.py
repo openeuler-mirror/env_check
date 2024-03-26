0804911ca819b653125640b5e4ceeeeef8a5bc4c
wangdong <wangdong_yewu@cmss.chinamobile.com>
2024-03-26 14:28:54 +0800

    add accept command for print related

---
+++ sut/env_check/check_actor/print_related_actor.py
@@ -0,0 +1,6 @@
+from sut.env_check.check_actor.oe_base_actor import OeBaseActor
+
+class OeAcceptActor(OeBaseActor):
+    name = "OeAcceptActor"
+    description = "Test the accept cmmand of the openEuler system."
+    sh_file = "05_print_related/oe_test01_accept.sh"
