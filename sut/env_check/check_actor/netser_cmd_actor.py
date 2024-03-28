ba70178c8e8fca5d0810386cab9a3cfb8c783fbf
dongchao <dongchao_yewu@cmss.chinamobile.com>
2024-03-28 17:00:45 +0800

    add code for netser_cmd_actor.py

---
+++ sut/env_check/check_actor/netser_cmd_actor.py
@@ -0,0 +1,20 @@
+class OeIplinkActor(OeBaseActor):
+    name = "OeIplinkActor"
+    description = "Test the iplink command of the openEuler system."
+    sh_file = '17_advnet_cmd_directories/oe_test01_iplink.sh'
+class OeIpaddrActor(OeBaseActor):
+    name = "OeIpaddrActor"
+    description = "Test the iplink command of the openEuler system."
+    sh_file = '17_advnet_cmd_directories/oe_test02_ipaddr.sh'
+class OeIprouteActor(OeBaseActor):
+    name = "OeIprouteActor"
+    description = "Test the iplink command of the openEuler system."
+    sh_file = '17_advnet_cmd_directories/oe_test03_iproute.sh'
+class OeIpneighActor(OeBaseActor):
+    name = "OeIpneighActor"
+    description = "Test the iplink command of the openEuler system."
+    sh_file = '17_advnet_cmd_directories/oe_test04_ipneigh.sh'	
+class OeIptablesActor(OeBaseActor):
+    name = "OeIptablesActor"
+    description = "Test the iplink command of the openEuler system."
+    sh_file = '17_advnet_cmd_directories/oe_test05_iptables.sh'
