1463e505c7486b95041fb0ee777e6d3d3f5292fc
dongchao <dongchao_yewu@cmss.chinamobile.com>
2024-05-08 17:04:23 +0800

    add code for oe_test18_lsblk02.sh

---
+++ sut/env_check/smoke_test/local_sh/09_disk_and_file_system/oe_test18_lsblk02.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/bash -x
+
+# Create: 2024-01-25
+
+OET_PATH=$(
+    cd "$(dirname "$0")" || exit 1
+    pwd
+)
+source "$OET_PATH/../../common/common_lib.sh"
+# 环境准备
+function pre_test() {
+    LOG_INFO "Start to prepare the test environment."
+    OLD_LANG=$LANG
+    export LANG=en_US.UTF-8
+    LOG_INFO "End to prepare the test environment."
+}
+# 用例执行
+function run_test() {
+    LOG_INFO "Start testing..."
+   
+    lsblk -P
+    
+    CHECK_RESULT $?
+
+    lsblk -S
+
+    CHECK_RESULT $?
+
+    lsblk -a
+
+    CHECK_RESULT $? 
+    LOG_INFO "Finish test!"
+}
+# 环境清理
+function post_test() {
+    LOG_INFO "Start to restore the test environment."
+    export LANG=${OLD_LANG}
+    LOG_INFO "End to restore the test environment."
+}
