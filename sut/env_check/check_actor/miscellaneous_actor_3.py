c7bb8fc2ea44cd7633c45137fe255d60670b0c19
zengyifenggitee <zengyifeng_yewu@cmss.chinamobile.com>
2024-05-09 10:39:55 +0800

    add chronyc actor

---
+++ sut/env_check/check_actor/miscellaneous_actor_3.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+from sut.env_check.check_actor.oe_base_actor import OeBaseActor
+
+class OeChronycActor(OeBaseActor):
+    name = "OeChronycActor"
+    description = "Test the chronyc command of the openEuler system."
+    sh_file = '99_miscellaneous/oe_test08_chronyc.sh'
