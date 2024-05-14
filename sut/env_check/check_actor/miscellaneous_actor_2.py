51eccbbce92239462bf968844ec19f5bb2f079fc
zengyifenggitee <zengyifeng_yewu@cmss.chinamobile.com>
2024-05-14 14:34:17 +0800

    add clockdiff actor

--- sut/env_check/check_actor/miscellaneous_actor_2.py
+++ sut/env_check/check_actor/miscellaneous_actor_2.py
@@ -11,3 +11,8 @@ class OeChrtActor(OeBaseActor):
     name = "OeChrtActor"
     description = "Test the chrt command of the openEuler system."
     sh_file = '99_miscellaneous/oe_test13_chrt.sh'
+
+class OeClockdiffActor(OeBaseActor):
+    name = "OeClockdiffActor"
+    description = "Test the clockdiff command of the openEuler system."
+    sh_file = '99_miscellaneous/oe_test19_clockdiff.sh'
