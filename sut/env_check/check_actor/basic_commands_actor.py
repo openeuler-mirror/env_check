b946230be25c72ad462f7e440760d5ff638b9954
wangdong <wangdong_yewu@cmss.chinamobile.com>
2024-05-23 14:04:16 +0800

    add blkdiscard command

--- sut/env_check/check_actor/basic_commands_actor.py
+++ sut/env_check/check_actor/basic_commands_actor.py
@@ -215,3 +215,8 @@ class OeBase64Actor(OeBaseActor):
     name = "OeBase64Actor"
     description = "Test the base64 command of the openEuler system."
     sh_file = '06_basic_commands/oe_test46_base64.sh'
+
+class OeBlkdiscardActor(OeBaseActor):
+    name = "OeBlkdiscardActor"
+    description = "Test the blkdiscard command of the openEuler system."
+    sh_file = '06_basic_commands/oe_test47_blkdiscard.sh'
