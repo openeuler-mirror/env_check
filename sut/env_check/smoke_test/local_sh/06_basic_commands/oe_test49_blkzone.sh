91ff37551c4b7d22f4e589e169afe086b2775c45
wangdong <wangdong_yewu@cmss.chinamobile.com>
2024-05-27 10:24:47 +0800

    add blkzone command

---
+++ sut/env_check/smoke_test/local_sh/06_basic_commands/oe_test49_blkzone.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/bash
+# Create: 2024-05-27 10:08:07
+# Auther: wangdong
+# blkzone - run zone command on a device
+
+
+OET_PATH=$(
+    cd "$(dirname "$0")" || exit 1
+    pwd
+)
+source "$OET_PATH/../../common/common_lib.sh"
+
+# 环境准备
+function pre_test() {
+    LOG_INFO "Start to prepare the test environment."
+    OLD_LANG=$LANG
+    export LANG=en_US.UTF-8
+    LOG_INFO "End to prepare the test environment."
+}
+
+# 用例执行
+function run_test() {
+    LOG_INFO "Start testing..."
+    # 检查 blkzone 命令是否安装。
+    if ! command -v blkzone &> /dev/null; then
+        LOG_WARN "blkzone command is not installed"
+        CHECK_RESULT $? 0 0
+    fi
+
+    blkzone -h
+    CHECK_RESULT $?
+
+    LOG_INFO "Finish test!"
+}
+
+# 环境清理
+function post_test() {
+    LOG_INFO "Start to restore the test environment."
+    export LANG=${OLD_LANG}
+    LOG_INFO "End to restore the test environment."
+}
+
+main $@
