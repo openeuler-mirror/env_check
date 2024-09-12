9aac4da0b24f4b5a208c804f8ad6c1ff0008a987
wangdong_cmcc <wangdong_yewu@cmss.chinamobile.com>
2024-09-12 22:33:58 +0800

    add bzegrep command

--- sut/env_check/check_actor/basic_commands_actor.py
+++ sut/env_check/check_actor/basic_commands_actor.py
@@ -310,3 +310,8 @@ class OeBzdiffctor(OeBaseActor):
     name = "OeBzdiffActor"
     description = "Test the bzdiff command of the openEuler system."
     sh_file = '06_basic_commands/oe_test66_bzdiff.sh'
+
+class OeBzegrepctor(OeBaseActor):
+    name = "OeBzegrepActor"
+    description = "Test the bzegrep command of the openEuler system."
+    sh_file = '06_basic_commands/oe_test67_bzegrep.sh'
