cb1ad42ea37512064cdcc51bf5c1177b13e5d6d1
wangdong_cmcc <wangdong_yewu@cmss.chinamobile.com>
2024-10-15 08:31:07 +0800

    add bzip2recover command check for euler os

--- sut/env_check/check_actor/basic_commands_actor_2.py
+++ sut/env_check/check_actor/basic_commands_actor_2.py
@@ -6,3 +6,8 @@ class OeBzip2Actor(OeBaseActor):
     description = "Test the bzip2 command of the openEuler system."
     sh_file = '06_basic_commands_2/oe_test01_bzip2.sh'
 
+class OeBzip2recoverActor(OeBaseActor):
+    name = "OeBzip2recoverActor"
+    description = "Test the bzip2recover command of the openEuler system."
+    sh_file = '06_basic_commands_2/oe_test02_bzip2recover.sh'
+
