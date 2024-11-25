bd0273cf104793c871f2ee39a444c0799293444f
wangdong_cmcc <wangdong_yewu@cmss.chinamobile.com>
2024-11-25 10:49:17 +0800

    add file command check for os

--- sut/env_check/check_actor/text_process_actor.py
+++ sut/env_check/check_actor/text_process_actor.py
@@ -190,3 +190,8 @@ class OeJournalctlActor(OeBaseActor):
     description = "Test the journalctl cmmand of the openEuler system."
     sh_file = "03_text_process/oe_test39_journalctl.sh"
 
+class OeFileActor(OeBaseActor):
+    name = "OeFileActor"
+    description = "Test the file cmmand of the openEuler system."
+    sh_file = "03_text_process/oe_test41_file.sh"
+
